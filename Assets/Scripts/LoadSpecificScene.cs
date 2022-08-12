f19ee2c3a040b27842d685e29357f85b6f86319c
EstelleUbi <estelle.tourillon@ubidreams.com>
2022-08-12 17:08:54 +0200

    feat: ajouter des niveaux

---
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    public string SceneName;
+    public Animator fadeSystem;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(loadNextScene());
+        }
+    }
+
+    public IEnumerator loadNextScene()
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneName);
+    }
+}
